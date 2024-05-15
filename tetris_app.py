d29d4bc7298ff9f080bac0cbd7cb4d90578a9d0f
Alexa Adams <bookwormlex@gmail.com>
2024-05-14 23:10:52 -0700

    Initial commit for tetris game launch file

---
+++ tetris_app.py
@@ -0,0 +1,20 @@
+# Tetris App Pygame class
+# main module for launching game
+
+import pygame
+
+class TetrisApp:
+    """ """
+
+    def __init__(self, log_level):
+        """ """
+
+
+    def run_game(self):
+        """ """
+
+
+if __name__ == '__main__':
+    # create instance of game object, and run the game
+    ta = TetrisApp("DEBUG")
+    ta.run_game()
