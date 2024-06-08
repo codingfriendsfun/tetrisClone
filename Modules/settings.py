93a86c36929d4d85d365784d627f488a96f52987
Kerowyn202 <noellelaurinriley@gmail.com>
2024-06-08 12:06:41 -0700

    Added screen settings and areas to initialize settings for different parts of the game

--- Modules/settings.py
+++ Modules/settings.py
@@ -1,9 +1,27 @@
 # settings class for Tetris App
+import pygame
+
 
 class Settings:
     """A class to store all settings."""
 
     def __init__(self):
         """Initialize the game's settings."""
+        # Screen settings
+        self.screen_width = 800
+        self.screen_width = 600
+        # Set bg as black as placeholder - not sure if it matters given that we have the img
+        self.bg_color = (0, 0, 0)
+        self.bg_img = pygame.image.load("Resources\Bck.png")
+
+        # Area for tetramino settings
+
+        # Area for difficulty settings
+
+        # Area for score settings
+
+        # Area for music settings
+
+        # Area for game mode settings (maybe?)
 
         
