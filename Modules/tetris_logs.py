6390914d8d07ad287db680dd9da5f390c3eeedca
Kristin <knhbjr@gmail.com>
2024-06-18 20:16:31 -0500

    Definited TetrisLogger attributes.

--- Modules/tetris_logs.py
+++ Modules/tetris_logs.py
@@ -3,9 +3,16 @@
 import logging
 
 class TetrisLogger:
-    """ """
+    """Base logger for tetrisApp"""
 
-    def __init__(self):
-        """ """
+    def __init__(self, module_name, level):
+        """Initialize the logger and logging attributes."""
 
+        self.logger = logging.getLogger(module_name)
+        self.level = level
+
+        self.config_logger()
+        self.define_log_levels()
+
+        self.get_log_level(self.level)
         
