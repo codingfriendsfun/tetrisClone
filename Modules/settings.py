3c66fd688c612a875659b7bf47b064fdd6fce253
Kerowyn202 <noellelaurinriley@gmail.com>
2024-07-18 21:02:43 -0700

    Imported and initialized logger into Settings class

--- Modules/settings.py
+++ Modules/settings.py
@@ -1,15 +1,21 @@
 # settings class for Tetris App
 import pygame
 
+from tetris_logs import TetrisLogger
+
 
 class Settings:
     """A class to store all settings."""
 
     def __init__(self):
         """Initialize the game's settings."""
+        # Initialize logging
+        self.logs = TetrisLogger('__name__')
+        
         # Screen settings
         self.screen_width = 800
         self.screen_width = 600
+
         # Set bg as black as placeholder - not sure if it matters given that we have the img
         self.bg_color = (0, 0, 0)
         self.bg_img = pygame.image.load("Resources\Bck.png")
