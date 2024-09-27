2ebf7afcb02aa31608696a1ccd2688cd886e410d
Kristin <knhbjr@gmail.com>
2024-09-27 17:07:23 -0500

    Added tetris_game to attributes, grabbed window size, and added to the BG code.

--- Modules/game_ui.py
+++ Modules/game_ui.py
@@ -8,16 +8,19 @@ from tetris_logs import TetrisLogger
 class GameUI:
     """Main game board."""
 
-    def __init__(self):
+    def __init__(self, tetris_game):
         """Initialize GameUI attributes."""
 
         self.logs = TetrisLogger('__name__')
 
-        self.bg = 'Resources/Bck_revised.png'
+        # Get size of window
+        self.window_width, self.window_height = pygame.display.get_surface().get_size()
 
-        #self.pause_menu = 
+        # Configure and display game board.
+        self.bg = pygame.image.load('Resources/Bck_revised.png')
+        self.rect = self.bg.get_rect()
 
-        # Pause menu (seperate class?)
+        #self.pause_menu = Pause_Menu()
         
         # Define board edges
 
